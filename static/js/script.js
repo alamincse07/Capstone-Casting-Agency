7842eb975dfb561d10a9b7481e50fff33c98d063
Alamin <alamin@w3engineers.com>
2020-07-16 12:31:25 +0600

    #Fix  - permission based link enabled

--- static/js/script.js
+++ static/js/script.js
@@ -20,72 +20,73 @@ if (tokenUrl) {
     });
   }
 }
+function processPermission(){
 
-if (localStorage.getItem('token') && localStorage.getItem('permissions')) {
-  if (window.location.pathname === '/') {
-    document.getElementById('index-view').innerHTML= " "
-    //window.location.replace('/movies')
+  if (localStorage.getItem('token') && localStorage.getItem('permissions')) {
+    if (window.location.pathname === '/') {
+     null !== document.getElementById('index-view') ? document.getElementById('index-view').innerHTML = " ": ""
+      //window.location.replace('/movies')
+    }
+
+    // hide log in button if logged in
+    document.getElementById('loginButton') && document.getElementById('loginButton').remove();
+
+    // hide add actor button if no permissions
+    if (!localStorage.getItem('permissions').includes('write:actors')) {
+      document.getElementById('addActorButton') && document.getElementById('addActorButton').remove();
+    }
+
+    // hide add movie button if no permissions
+    if (!localStorage.getItem('permissions').includes('write:movies')) {
+      document.getElementById('addMovieButton') && document.getElementById('addMovieButton').remove();
+    }
+
+    // hide edit actor button if no permissions
+    if (!localStorage.getItem('permissions').includes('update:actors')) {
+      document.querySelectorAll('.editActorButton') && document.querySelectorAll('.editActorButton').forEach(e => e.remove());
+    }
+
+    // hide edit movie button if no permissions
+    if (!localStorage.getItem('permissions').includes('update:movies')) {
+      document.querySelectorAll('.editMovieButton') && document.querySelectorAll('.editMovieButton').forEach(e => e.remove());
+    }
+
+    // hide delete actor button if no permissions
+    if (!localStorage.getItem('permissions').includes('delete:actors')) {
+      document.querySelectorAll('.deleteActorButton') && document.querySelectorAll('.deleteActorButton').forEach(e => e.remove());
+    }
+
+    // hide delete movie button if no permissions
+    if (!localStorage.getItem('permissions').includes('delete:movies')) {
+      document.querySelectorAll('.deleteMovieButton') && document.querySelectorAll('.deleteMovieButton').forEach(e => e.remove());
+    }
   }
+   else {
 
-  // hide log in button if logged in
-  document.getElementById('loginButton').remove();
-
-  // hide add actor button if no permissions
-  if (!localStorage.getItem('permissions').includes('write:actors')) {
+    document.querySelectorAll('.card-footer') && document.querySelectorAll('.card-footer').forEach(e => e.remove());
     document.getElementById('addActorButton').remove();
-  }
-
-  // hide add movie button if no permissions
-  if (!localStorage.getItem('permissions').includes('write:movies')) {
     document.getElementById('addMovieButton').remove();
-  }
+    document.getElementById('logoutButton').remove();
+    document.getElementById('movieLink').remove();
+    document.getElementById('actorLink').remove();
 
-  // hide edit actor button if no permissions
-  if (!localStorage.getItem('permissions').includes('update:actors')) {
-    document.querySelectorAll('.editActorButton').forEach(e => e.remove());
   }
-
-  // hide edit movie button if no permissions
-  if (!localStorage.getItem('permissions').includes('update:movies')) {
-    document.querySelectorAll('.editMovieButton').forEach(e => e.remove());
-  }
-
-  // hide delete actor button if no permissions
-  if (!localStorage.getItem('permissions').includes('delete:actors')) {
-    document.querySelectorAll('.deleteActorButton').forEach(e => e.remove());
-  }
-
-  // hide delete movie button if no permissions
-  if (!localStorage.getItem('permissions').includes('delete:movies')) {
-    document.querySelectorAll('.deleteMovieButton').forEach(e => e.remove());
-  }
-} else {
-  
-  // document.querySelectorAll('.card-footer').forEach(e => e.remove());
-  // document.getElementById('addActorButton').remove();
-  // document.getElementById('addMovieButton').remove();
-  // document.getElementById('logoutButton').remove();
-  // document.getElementById('movieLink').remove();
-  // document.getElementById('actorLink').remove();
-  // if (window.location.pathname !== '/') {
-  //   window.location.replace('/')
-  // }
 }
 
-// show and hide modal
-let target;
-// document.querySelector(".modal-button").forEach(function(el) {
-//   el.addEventListener("click", function() {
-//     target = document.querySelector(el.getAttribute("data-target"));
-//     target.classList.add("is-active");
-//     target.querySelector(".modal-close").addEventListener("click",   function() {
-//       target.classList.remove("is-active");
-//     });
-//   });
+//  else {
+  
+//   // document.querySelectorAll('.card-footer').forEach(e => e.remove());
+//   // document.getElementById('addActorButton').remove();
+//   // document.getElementById('addMovieButton').remove();
+//   // document.getElementById('logoutButton').remove();
+//   // document.getElementById('movieLink').remove();
+//   // document.getElementById('actorLink').remove();
+//   // if (window.location.pathname !== '/') {
+//   //   window.location.replace('/')
+//   // }
+// }
 
-//   document.getElementById("editActorModal").style.display = 'block';
-//   document.getElementById("editMovieModal").style.display = 'none';
-// });
+
 var addMovie = document.getElementById("addMovieButton");
 if (addMovie) {
   addMovie.onclick = function (e) {
@@ -110,10 +111,7 @@ function closemodal() {
 });
  
 }
-// document.querySelector(".modal-close").addEventListener("click", function () {
-//   console.log('cl')
-//    document.querySelector('.modal').style.display='none';
-// });
+
 
 // reusable function for POST & PATCH requests
 const sendData = async (url, data, method) => {
@@ -183,11 +181,10 @@ const submitMovie = async () => {
 // set form values for easier editing
 const setFormValues = (formName = '', data = {}) => {
   if (formName === 'editMovieForm') {
-    document.getElementById('editMovieForm_id').value = data.id;
-    document.getElementById('editMovieForm_title').value = data.title;
-    document.getElementById('editMovieForm_release_date').value = data.release_date;
-    // document.forms['editMovieForm']['title'].value = data.title;
-    // document.forms['editMovieForm']['release_date'].value = data.release_date;
+
+    document.forms['editMovieForm']['id'].value = data.id;
+    document.forms['editMovieForm']['title'].value = data.title;
+    document.forms['editMovieForm']['release_date'].value = data.release_date;
     document.getElementById("editActorModal").style.display = 'none';
     document.getElementById("editMovieModal").style.display = 'block';
 
@@ -197,7 +194,6 @@ const setFormValues = (formName = '', data = {}) => {
     document.forms['editActorForm']['name'].value = data.name;
     document.forms['editActorForm']['gender'].value = data.gender;
 
-    console.log(document.forms['editActorForm']['id'].value);
     document.getElementById("editActorModal").style.display = 'block';
     document.getElementById("editMovieModal").style.display = 'none';
   }
@@ -305,6 +301,7 @@ function loadActor(){
   ).then(function (response) {
     return response.text().then(function (text) {
       document.getElementById('content').innerHTML = text;
+      processPermission();
     });
   });
 }
@@ -321,6 +318,7 @@ function loadMovie(){
   ).then(function (response) {
     return response.text().then(function (text) {
       document.getElementById('content').innerHTML = text;
+      processPermission();
     });
   });
 }
@@ -330,3 +328,6 @@ document.getElementById("actorLink").onclick = function (e) {
   e.preventDefault();
   loadActor();
 };
+
+
+processPermission();
